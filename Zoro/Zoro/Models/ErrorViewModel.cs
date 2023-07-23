39656d9239c9d35dd4a7c8674cdfda53c79d010f
Mitko-Dimitrow <mitimitko@abv.bg>
2023-07-23 22:39:12 +0300

    First commit
    Created the project repo

---
+++ Zoro/Zoro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zoro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
