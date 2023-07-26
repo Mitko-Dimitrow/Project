19a4410b3ab6d21bb2190c49a8b13367f3980a05
Mitko-Dimitrow <mitimitko@abv.bg>
2023-07-26 21:52:05 +0300

    Set up the connection to database
    Set up the connection to database

---
+++ Zoro/Zoro/Models/AnimeInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Zoro.Models
+{
+    public class AnimeInfo
+    {
+        public Guid Id { get; set; }
+
+        public string AnimeName { get; set; }
+
+        public string genre { get; set; }
+
+        public string Status { get; set; }
+
+
+
+    }
+}
--- Zoro/Zoro/Program.cs
+++ Zoro/Zoro/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Zoro.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,5 +34,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
