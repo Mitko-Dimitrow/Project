4fc2347d67f43366352c25f0fe20d30f307dfb5f
Mitko-Dimitrow <mitimitko@abv.bg>
2023-08-16 22:11:19 +0300

    Watch controller created
    Watch controller created

---
+++ Zoro/Zoro/Controllers/WatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zoro.Controllers
+{
+    public class WatchController : BaseController
+    {
+        public IActionResult Watch()
+        {
+            return View();
+        }
+    }
+}
