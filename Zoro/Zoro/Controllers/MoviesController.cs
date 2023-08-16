9a590a043b62f5708dc41d91330d596a897fd2cc
Mitko-Dimitrow <mitimitko@abv.bg>
2023-08-16 21:36:16 +0300

    Created More Controllers
    Created More Controllers

---
+++ Zoro/Zoro/Controllers/MoviesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zoro.Controllers
+{
+    public class MoviesController : BaseController
+    {
+        public IActionResult Movies()
+        {
+            return View();
+        }
+    }
+}
